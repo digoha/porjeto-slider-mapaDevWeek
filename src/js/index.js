ae8e693ecac9183f1093056259a84f4e240e3867
Rodrigo H. Albernaz <90360514+digoha@users.noreply.github.com>
2022-10-05 01:27:38 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,3 @@
-/*
-Objetivo 1 - quando clicarmos na seta de avançar temos que mostrar o proximo cartão
-    passo 1 - dar um jeito de pegar o codigo HTML da seta avançar
-    passo 2 - dar um jeito de identificar o clique do usuario na seta avancar
-    passo 3 - fazer aparecer o proximo cartao
-    passo 4 - buscar o cartao que esta selecionado e esconder
-
-Objetivo 2 - igual o 1, porem com a seta de voltar
-*/
-
 const btnAvancar = document.getElementById("btn-avancar");
 const btnVoltar = document.getElementById("btn-voltar");
 
@@ -27,13 +17,18 @@ function mostrarCartao(indiceCartao){
 btnAvancar.addEventListener("click", function(){
 
     if(cartaoAtual === cartoes.length - 1){
-        return;
+        //return;
+        cartaoAtual = 0;
+    }
+    else
+    {
+        cartaoAtual++;
     }
 
-    
+
     esconderCartaoSelecionado();
 
-    cartaoAtual++;
+
     mostrarCartao(cartaoAtual);
 
 });
@@ -41,13 +36,14 @@ btnAvancar.addEventListener("click", function(){
 btnVoltar.addEventListener("click", function(){
 
     if(cartaoAtual === 0){
-        return;
+        //return;
+        cartaoAtual = cartoes.length - 1;
     }
-
+    else
+    {
+        cartaoAtual--;
+    }
     esconderCartaoSelecionado();
 
-    cartaoAtual--;
     mostrarCartao(cartaoAtual);
 });
-
-
